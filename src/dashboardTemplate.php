bd3f7efac0220d324da49415fed57b10a1e8ca2d
Evangelista Grace <evangrace98@gmail.com>
2019-09-08 15:08:32 +0800

    Create dashboardTemplate.php

---
+++ src/dashboardTemplate.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<?php include 'head.php'?>
+<title>TITLE</title>
+
+<body>
+   
+    <?php include 'navbarDashboard.php'?>
+    <div class="container-fluid my-container offset-container">
+
+        <div class="row">
+            <!-- SIDEBAR -->
+            <?php include 'sidebarDashboard.php'?>
+
+            <!-- MAIN CONTENT -->
+
+    <?php include 'footer.php' ?>
+
+</body>
+
+</html>
