e31be4d802ea9b98855485bf6ba72becd126c87a
Evangelista Grace <evangrace98@gmail.com>
2019-09-08 15:06:49 +0800

    dashboard sidebar

--- src/dashboard.php
+++ src/dashboard.php
@@ -9,31 +9,11 @@
 <?php include 'navbarDashboard.php'?>
     <div class="container-fluid my-container offset-container">
 
-        <!-- MAIN CONTENT    -->
         <div class="row">
             <!-- SIDEBAR -->
-            <div class="col-2-sidebar sidebar collapsed position-fixed">
+            <?php include 'sidebarDashboard.php'?>
 
-                <!-- close button -->
-                <div class="close"><a class="toggleBtn" onclick="toggleSidebar()"></a></div>
-
-                <!-- profile section -->
-                <div class="profile"><img src="../assets/profile.jpg" alt="">
-                    <div class="desc">@chevvycherokee</div>
-                </div>
-
-                <!-- menu -->
-                <ul class="menu">
-                    <li class="menu-item" id="menuItem1"><a href="#" title="dashboard"></a></li>
-                    <li class="menu-item" id="menuItem2"><a href="#" title="budgets"></a></li>
-                    <li class="menu-item" id="menuItem3"><a href="#" title="expenses"></a></li>
-                    <li class="menu-item" id="menuItem4"><a href="#" title="reminders"></a></li>
-                    <li class="menu-item" id="menuItem5"><a href="#" title="groups"></a></li>
-                    <li class="menu-item" id="menuItem6"><a href="#" title="report"></a></li>
-                    <li class="menu-item" id="menuItem7"><a href="#" title="settings"></a></li>
-                </ul>
-            </div>
-
+            <!-- MIAIN CONTENT -->
             <div class="col-10-body collapsed">
                 <div class="row">
                     <div class="card" style="width: 18rem;">
---
+++ src/sidebarDashboard.php
@@ -0,0 +1,20 @@
+<?php
+
+    echo '<div class="col-2-sidebar sidebar collapsed position-fixed">
+    <div class="close"><a class="toggleBtn" onclick="toggleSidebar()"></a></div>
+
+    <div class="profile"><img src="../assets/profile.jpg" alt="">
+        <div class="desc">@chevvycherokee</div>
+    </div>
+
+    <ul class="menu">
+        <li class="menu-item" id="menuItem1"><a href="#" title="dashboard"></a></li>
+        <li class="menu-item" id="menuItem2"><a href="#" title="budgets"></a></li>
+        <li class="menu-item" id="menuItem3"><a href="#" title="expenses"></a></li>
+        <li class="menu-item" id="menuItem4"><a href="#" title="reminders"></a></li>
+        <li class="menu-item" id="menuItem5"><a href="#" title="groups"></a></li>
+        <li class="menu-item" id="menuItem6"><a href="#" title="report"></a></li>
+        <li class="menu-item" id="menuItem7"><a href="#" title="settings"></a></li>
+    </ul>
+    </div>';
+?>
