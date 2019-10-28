413c482dae98eed87b42b2bb95c2c7446dc754b7
Evangelista Grace <grace.evangrace@gmail.com>
2019-10-28 21:08:02 +0800

    .

--- src/settings.php
+++ src/settings.php
@@ -30,8 +30,10 @@ $income = "Add income...";
                             <h5 class="text-left"><strong>Income</strong></h5>
                             <table class='table table-condensed settings'>
                                 <tr>
-                                    <td style="width:70%"><input class="form-control" name="income" id="income" type="text" value="<?php echo $income ?>"></td>
-                                    <td style="width:20%"><button class="btn btn-block btn-info">Add Income</button></td>
+                                    <td style="width:70%"><input class="form-control" name="income" id="income"
+                                            type="text" value="<?php echo $income ?>"></td>
+                                    <td style="width:20%"><button class="btn btn-block btn-info">Add Income</button>
+                                    </td>
                                 </tr>
                             </table>
                             <hr>
@@ -65,12 +67,14 @@ $income = "Add income...";
                             <h5 class="text-left"><strong>Reminders</strong></h5>
                             <table class='table table-condensed settings'>
                                 <tr>
-                                    <td><input type="checkbox" class="checkbox"><div class="pseudo-checkbox"></div>
+                                    <td><input type="checkbox" class="checkbox">
+                                        <div class="pseudo-checkbox"></div>
                                     </td>
 
                                     <td>Allow pop-up reminders</td>
 
-                                    <td><input type="checkbox" class="checkbox"><div class="pseudo-checkbox"></div>
+                                    <td><input type="checkbox" class="checkbox">
+                                        <div class="pseudo-checkbox"></div>
                                     </td>
 
                                     <td>Allow push notifications</td>
@@ -83,25 +87,28 @@ $income = "Add income...";
 
                             <h5 class="text-left"><strong>Categories</strong></h5>
                             <table class='table table-condensed settings2'>
-                                <?php  $query = pg_query("SELECT * FROM categories WHERE username = '".$_SESSION['username']."' "); ?>
-                                <?php while($result = pg_fetch_array($query)){ ?>
-                                    <tr>
-                                       <td><?php echo $result['categoryname'] ?></td>
-                                        <td>
-                                            <a href="#"><i class="fas fa-edit text-primary"></i></a>
-                                            <a href="#"><i class="far fa-trash-alt text-danger"></i></a>
-                                        </td>
-                                    </tr>
-                                <?php } ?>
+
                                 <tr>
-                                    <td>
+                                    <td style="width: 90%">
                                         <input class="form-control" type="text" name="newCategory" id="newCategory"
                                             placeholder="Enter a new category...">
                                     </td>
-                                    <td>
+                                    <td style="width: 10%">
                                         <a href="" class="btn btn-danger btn-round">+</a>
                                     </td>
                                 </tr>
+
+                                <?php  $query = pg_query("SELECT * FROM categories WHERE username = '".$_SESSION['username']."' "); ?>
+                                <?php while($result = pg_fetch_array($query)){ ?>
+                                <tr>
+                                    <td style="width: 90%"><?php echo $result['categoryname'] ?></td>
+                                    <td style="width: 10%">
+                                        <a href="#"><i class="fas fa-edit text-primary"></i></a>
+                                        <a href="#"><i class="far fa-trash-alt text-danger"></i></a>
+                                    </td>
+                                </tr>
+                                <?php } ?>
+
                             </table>
 
                         </div>
