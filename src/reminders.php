32b308aeae83d6b70fbe584caffd9349df5de8fa
Evangelista Grace <grace.evangrace@gmail.com>
2019-11-14 03:28:46 +0800

    Update reminders.php

--- src/reminders.php
+++ src/reminders.php
@@ -3,6 +3,13 @@
 <?php 
 include 'head.php';
 require 'reminders-process.php';
+
+// initialize edit variables
+$reminderid = $_GET['edit-reminder'];
+$remindername = $_GET['reminder-name'];
+$remindercategory = $_GET['reminder-category'];
+$reminderamount = $_GET['reminder-amount'];
+
 ?>
 <title>My Budgets - BudgetTracker</title>
 
@@ -47,7 +54,7 @@ require 'reminders-process.php';
                                     </td>
                                     <td>RM <?php echo $reminder['reminderamount'] ?></td>
                                     <td>
-                                        <a href="#"><i class="fas fa-edit text-primary"></i></a>
+                                        <a href="reminders.php?edit-reminder=<?php echo $reminder['reminderid']?>&reminder-name=<?php echo $reminder['remindername']?>&reminder-category=<?php echo $reminder['categoryname']?>&reminder-amount=<?php echo $reminder['reminderamount']?>#editReminder"><i class="fas fa-edit text-primary"></i></a>
                                         <a href="reminders-process.php?del-reminder=<?php echo $reminder['reminderid']?>"><i class="far fa-trash-alt text-danger"></i></a>
                                     </td>
                                 </tr>
@@ -58,9 +65,9 @@ require 'reminders-process.php';
                 </div>
 
 
-                <!-- pop-up -->
                 <a class="btn btn-danger add-btn" href="#addReminder"><i class="fas fa-plus"></i></a>
 
+                <!-- add reminder -->
                 <div id="addReminder" class="overlay">
                     <div class="popup">
 
@@ -121,6 +128,76 @@ require 'reminders-process.php';
                         </div>
                     </div>
                 </div>
+
+
+                <!-- edit reminder -->
+                <div id="editReminder" class="overlay">
+                    <div class="popup">
+
+                        <div class="content"><a class="close" href="#">x</a>
+                            <h3 class="text-center mb-4 mt-4">Edit Reminder</h3>
+                            <form class="popup-form" action="reminders.php" method="POST">
+                                <div class="form-group">
+                                    <table>
+                                        <tr>
+                                            <td><label for="expenseTitle">Title</label></td>
+                                            <td>
+                                                <div class="input-group">
+                                                    <input type="text" class="form-control" name="reminder-name"
+                                                        id="reminderName" value="<?php echo $remindername ?>">
+                                                    <input type="hidden" name="reminder-id"  value=<?php echo $reminderid ?>>
+                                                </div>
+                                            </td>
+                                        </tr>
+                                    </table>
+                                </div>
+                                <div class="form-group">
+                                    <table>
+                                        <tr>
+                                            <td><label for="budgetCategory">Category</label></td>
+                                            <td><select class="selectpicker show-tick" data-style="btn-secondary"
+                                                    data-size="3" title="Pick a category" name="reminder-category">
+                                                    <?php $query = pg_query("SELECT * FROM categories WHERE username = '".$_SESSION['username']."' ")?>
+                                                    <?php while($result = pg_fetch_array($query)) : ?>
+                                                    <?php if($result['categoryname'] == $remindercategory): ?>
+                                                        <option value="<?php echo $result['categoryname'] ?>" selected>
+                                                        <?php echo $result['categoryname'] ?></option>
+                                                    <?php else: ?>
+                                                    <option value="<?php echo $result['categoryname'] ?>">
+                                                        <?php echo $result['categoryname'] ?></option>
+                                                    <?php endif ?>
+                                                    <?php endwhile ?>
+                                                </select></td>
+                                        </tr>
+                                    </table>
+                                </div>
+
+                                <div class="form-group">
+                                    <table>
+                                        <tr>
+                                            <td><label for="budgetAmount">Amount</label></td>
+                                            <td>
+                                                <div class="input-group">
+                                                    <div class="input-group-prepend">
+                                                        <span class="input-group-text">RM</span>
+                                                    </div>
+                                                    <input type="text" class="form-control text-right"
+                                                        aria-label="Amount (to the nearest ringgit)" value=<?php echo $reminderamount ?> name="reminder-amount">
+                                                </div>
+                                            </td>
+                                        </tr>
+                                    </table>
+
+                                </div>
+                                <div class="form-group">
+                                    <button class="btn btn-primary btn-lg btn-block" type="submit" name="edit-reminder">Edit reminder</button>
+
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+
 
             </div>
         </div>
