4576dc53c29521ff1e09abc5e4be46a947836e88
Evangelista Grace <grace.evangrace@gmail.com>
2020-01-05 01:16:22 +0800

    notifications UI

--- src/notifications.php
+++ src/notifications.php
@@ -1,37 +1,91 @@
 <!DOCTYPE html>
 <html lang="en">
 <?php include 'head.php'?>
-<title>TITLE</title>
+<title>My Notifications - BudgetTracker</title>
 
 <body>
-   
-    <?php include 'navbarDashboard.php'?>
-    <div class="container-fluid my-container offset-container">
 
-        <div class="row">
-            <!-- SIDEBAR -->
-            <?php include 'sidebarDashboard.php'?>
+  <?php include 'navbarDashboard.php'?>
+  <div class="container-fluid my-container offset-container">
 
-            <!-- MAIN CONTENT -->
-            <div class="col-10-body collapsed">
-                <div class="row">
-                    <?php
-                        $query = pg_query("SELECT * FROM notifications WHERE username = '".$_SESSION['username']."' ");
+    <div class="row">
+      <!-- SIDEBAR -->
+      <?php include 'sidebarDashboard.php'?>
 
-                    ?>
-
-                    <?php while($notification = pg_fetch_array($query)): ?>
-                        <div class="card">
-                        <div class="card-body">
-                            <div class="card-title"><?php echo $notification['notificationtitle']?></div>
-                        </div>
-                    </div>
-                    <?php  endwhile ?>
-                    
-                </div>
+      <!-- MAIN CONTENT -->
+      <div class="col-10-body collapsed">
+        <h1 class="title text-primary">My Notifications</h1>
+        <div class="row notification-container">
+          <p class="dismiss text-right"><a id="dismiss-all" href="#">Dimiss All</a></p>
+          <div class="card notification-card notification-invitation">
+            <div class="card-body">
+              <table>
+                <tr>
+                  <td style="width:70%">
+                    <div class="card-title">Jane invited you to join '<b>Familia</b>' group</div>
+                  </td>
+                  <td style="width:30%">
+                    <a href="#" class="btn btn-primary">View</a>
+                    <a href="#" class="btn btn-danger dismiss-notification">Dismiss</a>
+                  </td>
+                </tr>
+              </table>
             </div>
+          </div>
 
-    <?php include 'footer.php' ?>
+          <div class="card notification-card notification-warning">
+            <div class="card-body">
+              <table>
+                <tr>
+                  <td style="width:70%">
+                    <div class="card-title">Your expenses for '<b>Groceries</b>' has exceeded its budget</div>
+                  </td>
+                  <td style="width:30%">
+                    <a href="#" class="btn btn-primary">View</a>
+                    <a href="#" class="btn btn-danger dismiss-notification">Dismiss</a>
+                  </td>
+                </tr>
+              </table>
+            </div>
+          </div>
+
+          <div class="card notification-card notification-danger">
+            <div class="card-body">
+              <table>
+                <tr>
+                  <td style="width:70%">
+                    <div class="card-title">Insufficient budget to create '<b>Clothing</b>' budget category</div>
+                  </td>
+                  <td style="width:30%">
+                    <a href="#" class="btn btn-primary">View</a>
+                    <a href="#" class="btn btn-danger dismiss-notification">Dismiss</a>
+                  </td>
+                </tr>
+              </table>
+            </div>
+          </div>
+
+          <div class="card notification-card notification-reminder">
+            <div class="card-body">
+              <table>
+                <tr>
+                  <td style="width:70%">
+                    <div class="card-title">You have <b>2</b> upcoming payment(s) this week</div>
+                  </td>
+                  <td style="width:30%">
+                    <a href="#" class="btn btn-primary">View</a>
+                    <a href="#" class="btn btn-danger dismiss-notification">Dismiss</a>
+                  </td>
+                </tr>
+              </table>
+            </div>
+          </div>
+
+
+        </div>
+      </div>
+
+      <?php include 'footer.php' ?>
 
 </body>
 
