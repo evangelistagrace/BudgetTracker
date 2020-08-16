8ba19af04b58256d809056b7c5f43a50cac1a225
Grace <evangrace98@gmail.com>
2020-08-17 02:10:01 +0800

    heroku database setup

--- src/config.php
+++ src/config.php
@@ -1,6 +1,6 @@
 <?php
 
-$db = pg_connect("host=localhost port=5432 dbname=postgres user=postgres password=413506");
+$db = pg_connect("host=ec2-3-215-207-12.compute-1.amazonaws.com port=5432 dbname=dbu3gq1bkmrnms user=nywagreogodxif password=5bf2e2ccc94353a3fd82b5f6f585f4792ce1e61e25f18cacf3b35fa33245b74b");
 session_start();
 
 ?>
